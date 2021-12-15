61bc0c8e3359415f91c7496eb7b40c6a1274ac95
etienne0101 <79003454+etienne0101@users.noreply.github.com>
2021-12-15 17:37:54 +0100

    fusion INGRES back

---
+++ signup-back/app/policies/enrollment/api_ingres_policy.rb
@@ -0,0 +1,15 @@
+class Enrollment::ApiIngresPolicy < EnrollmentPolicy
+    def permitted_attributes
+      res = super
+  
+      res.concat([
+        scopes: [
+          :donnees_ingres_nomenclatures,
+          :donnees_ingres_noyau
+        ]
+      ])
+  
+      res
+    end
+  end
+  
